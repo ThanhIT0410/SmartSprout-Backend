d8ef2e672a9e9eb9e0be84484c7b4163260c25c4
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-05-06 11:44:05 +0700

    Initializing Spring Boot

---
+++ src/main/java/org/smartsproutbackend/SmartSproutBackendApplication.java
@@ -0,0 +1,13 @@
+package org.smartsproutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSproutBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSproutBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/smartsproutbackend/SmartSproutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smartsproutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSproutBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
