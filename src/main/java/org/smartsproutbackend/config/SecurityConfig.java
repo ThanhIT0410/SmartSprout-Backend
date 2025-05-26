1145b022a2a6e5aca9444d6bf47b00cf87984a49
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-05-27 04:08:43 +0700

    Hotfix: cors for websocket

--- src/main/java/org/smartsproutbackend/config/SecurityConfig.java
+++ src/main/java/org/smartsproutbackend/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        config.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
