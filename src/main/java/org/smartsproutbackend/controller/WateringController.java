56bc2ae7068c2e56f9a438ba6d47be3de227280f
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-05-27 07:29:41 +0700

    Add JWT tokens check to watering APIs

--- src/main/java/org/smartsproutbackend/controller/WateringController.java
+++ src/main/java/org/smartsproutbackend/controller/WateringController.java
@@ -3,14 +3,18 @@ package org.smartsproutbackend.controller;
 import org.smartsproutbackend.dto.WateringPlanRequest;
 import org.smartsproutbackend.dto.WateringTriggerRequest;
 import org.smartsproutbackend.entity.WateringPlan;
+import org.smartsproutbackend.service.AccessControlService;
 import org.smartsproutbackend.service.PlanSavingService;
+import org.smartsproutbackend.service.TokenService;
 import org.smartsproutbackend.service.WateringService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/watering")
@@ -22,26 +26,71 @@ public class WateringController {
     @Autowired
     private PlanSavingService planSavingService;
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private AccessControlService accessControlService;
+
     @PostMapping("/immediate")
-    public ResponseEntity<String> immediatelyTriggerWatering(@RequestBody WateringTriggerRequest request) {
-        wateringService.triggerWatering(request.getDeviceId(), request.getDeviceName(), request.getDuration());
-        return ResponseEntity.ok("Watering successfully");
+    public ResponseEntity<String> immediatelyTriggerWatering(@RequestHeader("Authorization") String authHeader,
+                                                             @RequestBody WateringTriggerRequest request) {
+        String token = authHeader.replace("Bearer ", "");
+        String username = tokenService.extractUsername(token);
+        if (!accessControlService.userHasAccessToDevice(username, request.getDeviceId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to this device");
+        }
+
+        try {
+            wateringService.triggerWatering(
+                    request.getDeviceId(),
+                    request.getDeviceName(),
+                    request.getDuration()
+            );
+            return ResponseEntity.ok("Watering successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
     }
 
     @PostMapping("/new-plan")
-    public ResponseEntity<WateringPlan> scheduleWatering(@RequestBody WateringPlanRequest request) {
+    public ResponseEntity<?> scheduleWatering(@RequestHeader("Authorization") String authHeader,
+                                              @RequestBody WateringPlanRequest request) {
+        String token = authHeader.replace("Bearer ", "");
+        String username = tokenService.extractUsername(token);
+        if (!accessControlService.userHasAccessToDevice(username, request.getDeviceId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to this device");
+        }
+
         WateringPlan plan = planSavingService.savePlan(request);
         return ResponseEntity.ok(plan);
+
     }
 
     @PutMapping("/change-plan/{planId}")
-    public ResponseEntity<WateringPlan> changeSchedule(@PathVariable Long planId, @RequestBody WateringPlanRequest request) {
+    public ResponseEntity<?> changeSchedule(@RequestHeader("Authorization") String authHeader,
+                                                       @PathVariable Long planId,
+                                                       @RequestBody WateringPlanRequest request) {
+        String token = authHeader.replace("Bearer ", "");
+        String username = tokenService.extractUsername(token);
+        if (!accessControlService.userHasAccessToDevice(username, request.getDeviceId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to this device");
+        }
+
         WateringPlan plan = planSavingService.updatePlan(planId, request);
         return ResponseEntity.ok(plan);
     }
 
     @GetMapping("/all-plan")
-    public ResponseEntity<List<WateringPlan>> getAllPlans(@RequestParam String deviceId) {
+    public ResponseEntity<?> getAllPlans(@RequestHeader("Authorization") String authHeader,
+                                                          @RequestParam String deviceId) {
+        String token = authHeader.replace("Bearer ", "");
+        String username = tokenService.extractUsername(token);
+        if (!accessControlService.userHasAccessToDevice(username, deviceId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to this device");
+        }
+
         List<WateringPlan> allPlans = planSavingService.getAllPlans(deviceId);
         return ResponseEntity.ok(allPlans);
     }
