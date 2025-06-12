19fb2d0844fe62911be4ac8fded2a07b233ac3cc
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-06-12 17:25:39 +0700

    Move recent message cleanup responsibility to database

--- src/main/java/org/smartsproutbackend/repository/RecentMessageRepository.java
+++ src/main/java/org/smartsproutbackend/repository/RecentMessageRepository.java
@@ -2,10 +2,6 @@ package org.smartsproutbackend.repository;
 
 import org.smartsproutbackend.entity.RecentMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface RecentMessageRepository extends JpaRepository<RecentMessage, Long> {
